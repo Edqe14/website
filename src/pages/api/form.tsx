c656cef7b9af89056be236397acdd6faa45d4aa6
Edqe14 <34704796+Edqe14@users.noreply.github.com>
2022-04-11 23:23:14 +0700

    remove form

--- src/pages/api/form.tsx
+++ src/pages/api/form.tsx
@@ -10,29 +10,31 @@ const limiter = rateLimit({
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export default async (req: Request, res: Response): Promise<any> => {
+  return res.status(403).json({ message: 'Closed' });
+
   // eslint-disable-next-line @typescript-eslint/no-empty-function
-  bodyParser.json()(req, res, () => {});
-  if (
-    !(await new Promise((resolve) => {
-      limiter(req, res, (e) => (e ? resolve(false) : resolve(true)));
-    }))
-  )
-    return;
+  // bodyParser.json()(req, res, () => {});
+  // if (
+  //   !(await new Promise((resolve) => {
+  //     limiter(req, res, (e) => (e ? resolve(false) : resolve(true)));
+  //   }))
+  // )
+  //   return;
 
-  if (req.method !== 'POST')
-    return res.status(405).json({ message: 'Only accepting "POST" requests' });
+  // if (req.method !== 'POST')
+  //   return res.status(405).json({ message: 'Only accepting "POST" requests' });
 
-  const { name, email, message } = req.body;
-  if (!name || !email || !message)
-    return res.status(400).json({ message: 'Missing elements' });
+  // const { name, email, message } = req.body;
+  // if (!name || !email || !message)
+  //   return res.status(400).json({ message: 'Missing elements' });
 
-  const response = await sendMail(email, name, message).catch((e) => {
-    res.status(500).json({ message: `Failed to send email: ${e.message}` });
-    return false;
-  });
+  // const response = await sendMail(email, name, message).catch((e) => {
+  //   res.status(500).json({ message: `Failed to send email: ${e.message}` });
+  //   return false;
+  // });
 
-  if (response)
-    return res
-      .status(200)
-      .json({ message: 'Successfully sent email to recipient' });
+  // if (response)
+  //   return res
+  //     .status(200)
+  //     .json({ message: 'Successfully sent email to recipient' });
 };
